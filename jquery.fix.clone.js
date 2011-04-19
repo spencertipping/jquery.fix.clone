d9014a7b7acfde5e5125ffc798c98c3b7b25ee28
Spencer Tipping <spencer@spencertipping.com>
2011-04-19 15:27:21 -0600

    Applied a <select> element fix from Piotr Przybyl -- now it should work properly with duplicate option values.

--- jquery.fix.clone.js
+++ jquery.fix.clone.js
@@ -5,14 +5,19 @@
 // jQuery's clone() method works in most cases, but it fails to copy the value of textareas and select elements. This patch replaces jQuery's clone() method with a wrapper that fills in the
 // values after the fact.
 
+// An interesting error case submitted by Piotr Przybył: If two <select> options had the same value, the clone() method would select the wrong one in the cloned box. The fix, suggested by Piotr
+// and implemented here, is to use the selectedIndex property on the <select> box itself rather than relying on jQuery's value-based val().
+
 (function (original) {
   jQuery.fn.clone = function () {
-    var result          = original.apply(this, arguments),
-        my_elements     = this.find('textarea, select'),
-        result_elements = result.find('textarea, select');
+    var result           = original.apply(this, arguments),
+        my_textareas     = this.find('textarea').add(this.filter('textarea')),
+        result_textareas = result.find('textarea').add(this.filter('textarea')),
+        my_selects       = this.find('select').add(this.filter('select')),
+        result_selects   = result.find('select').add(this.filter('select'));
 
-    for (var i = 0, l = my_elements.length; i < l; ++i)
-      $(result_elements[i]).val($(my_elements[i]).val());
+    for (var i = 0, l = my_textareas.length; i < l; ++i) $(result_textareas[i]).val($(my_textareas[i]).val());
+    for (var i = 0, l = my_selects.length;   i < l; ++i) result_selects[i].selectedIndex = my_textareas[i].selectedIndex;
 
     return result;
   };
