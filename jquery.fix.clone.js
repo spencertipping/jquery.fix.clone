e82d83639068d15a3077ea37893f04140fc1208f
Spencer Tipping <spencer@spencertipping.com>
2012-06-11 10:26:37 -0500

    Fixed a bad bug that caused immediate collection textareas and selects not to have their values propagated.

--- jquery.fix.clone.js
+++ jquery.fix.clone.js
@@ -12,9 +12,9 @@
   jQuery.fn.clone = function () {
     var result           = original.apply(this, arguments),
         my_textareas     = this.find('textarea').add(this.filter('textarea')),
-        result_textareas = result.find('textarea').add(this.filter('textarea')),
+        result_textareas = result.find('textarea').add(result.filter('textarea')),
         my_selects       = this.find('select').add(this.filter('select')),
-        result_selects   = result.find('select').add(this.filter('select'));
+        result_selects   = result.find('select').add(result.filter('select'));
 
     for (var i = 0, l = my_textareas.length; i < l; ++i) $(result_textareas[i]).val($(my_textareas[i]).val());
     for (var i = 0, l = my_selects.length;   i < l; ++i) result_selects[i].selectedIndex = my_selects[i].selectedIndex;
